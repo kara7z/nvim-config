94efaec02cd3c31561c715589b549631ffe9f2e5
kara7z <kara7z@users.noreply.github.com>
2026-07-12 22:33:16 +0100

    fix cpp.lua to extend mason opts instead of overriding

--- lua/plugins/cpp.lua
+++ lua/plugins/cpp.lua
@@ -1,9 +1,7 @@
 return {
   "williamboman/mason.nvim",
-  opts = {
-    ensure_installed = {
-      "clangd",
-      "codelldb",
-    },
-  },
+  opts = function(_, opts)
+    opts.ensure_installed = opts.ensure_installed or {}
+    vim.list_extend(opts.ensure_installed, { "clangd", "codelldb" })
+  end,
 }
